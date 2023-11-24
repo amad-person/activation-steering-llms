795fbda05b7725d44c96b8c7871c794229f7a644
Aadyaa Maddi <aadyaa.maddi@gmail.com>
2023-11-23 22:46:10 -0500

    Shuffle A B

--- data/more_qa_data_ab.py
+++ data/more_qa_data_ab.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset
 from itertools import product
 import json
+from random import shuffle
 
 A_B = []
 
@@ -11,14 +12,23 @@ for item in truthful_qa_rows:
     correct_answers = item["correct_answers"]
     incorrect_answers = item["incorrect_answers"]
     for answers in product(correct_answers, incorrect_answers):
+        answers = list(answers)
+        correct_answer = answers[0]
+        shuffle(answers)
+        if answers[0] == correct_answer:
+            answer_matching_behavior = "(A)"
+            answer_not_matching_behavior = "(B)"
+        else:
+            answer_matching_behavior = "(B)"
+            answer_not_matching_behavior = "(A)"
         full_question = (
             f"{question}\n\nChoices:\n(A) {answers[0]}\n(B) {answers[1]}"
         )
         A_B.append(
             {
                 "question": full_question,
-                "answer_matching_behavior": "(A)",
-                "answer_not_matching_behavior": "(B)",
+                "answer_matching_behavior": answer_matching_behavior,
+                "answer_not_matching_behavior": answer_not_matching_behavior,
             }
         )
 
